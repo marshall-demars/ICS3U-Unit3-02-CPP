14f563d98957efdf6606d9cc83b1e554ac6141e4
Marshall Demars <marshall.demars@mths.ca>
2022-10-13 15:22:02 +0000

    cpp

---
+++ guess.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2022 Marshall Demars All rights reserved
+//
+// Created by: Marshall Demars
+// Created on: October 2022
+// This program uses if statements to guess and random number
+//    with user input
+
+#include <iomanip>
+#include <iostream>
+
+int main() {
+    // this function calculates sum of two integers
+    int randomNumber;
+    const int CHOSEN_NUMBER = 5;
+
+    // input
+    std::cout << "Enter a random number between 1 and 9: ";
+    std::cin >> randomNumber;
+
+    // process and output
+    if (randomNumber == CHOSEN_NUMBER)
+
+    std::cout << "\nDone." << std::endl;
+}
